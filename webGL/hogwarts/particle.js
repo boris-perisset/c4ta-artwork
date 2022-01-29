7dc77a0adcc05ccb7a9100745913cab2e11fd3dd
boris-perisset <digital.noize@protonmail.com>
2022-01-29 23:09:24 +0100

    hogwart

---
+++ webGL/hogwarts/particle.js
@@ -0,0 +1,72 @@
+class Particle {
+    constructor(x, y, z){
+      this.size = random(4,20)
+      this.vec = new p5.Vector(x, y, z)
+      this.speedX = random(-1,1) * 1.2
+      this.speedY = random(-1,1) * 1.2
+      this.speedZ = random(-1,1) * 1.2
+    }
+  
+    showDot() {
+      push()
+      lightFalloff(1.2, 0, 0);
+      translate(this.vec.x, this.vec.y, this.vec.z)
+      // normalMaterial()
+      // emissiveMaterial(125, 0, 0)
+
+      ambientMaterial(mainColor)
+      noStroke()
+      sphere(this.size)
+      pop()
+    }
+
+    showFlare() {
+      let spotColor = mainLight
+      let spotPosition = this.vec
+      let spotDirection = this.vec
+      // spotLight(spotColor, spotPosition, spotDirection)
+      // pointLight(255, 255, 0, this.vec.x, this.vec.y, this.vec.z, this.vec.x+100, this.vec.y+100, this.vec.z+100, TAU, 5000)
+
+      push()
+      // lightFalloff(1, 0, 0);
+      translate(this.vec.x, this.vec.y, this.vec.z)
+      // pointLight(255, 255, 0, 0, 0, 0)
+      emissiveMaterial(mainLight)
+
+      // ambientMaterial(mainColor)
+      noStroke()
+      cone(this.size)
+      pop()
+    }
+  
+    updatePos(){
+      this.vec.x += this.speedX + random(-0.2,0.2)
+      this.vec.y += this.speedY + random(-0.2,0.2)
+      this.vec.z += this.speedZ + random(-0.2,0.2)
+  
+      if (this.vec.x <= -w/2) this.speedX *= -1
+      if (this.vec.x > w/2) this.speedX *= -1
+      if (this.vec.y <= -h/2) this.speedY *= -1
+      if (this.vec.y > h/2) this.speedY *= -1
+      if (this.vec.z <= -500) this.speedZ *= -1
+      if (this.vec.z > 500) this.speedZ *= -1
+    }
+  
+    //Mouse repels particles
+    //Also makes sure they don't leave the canvas
+    repel() {
+      this.vec.x = constrain(this.vec.x, -w/2, w/2);
+      this.vec.y = constrain(this.vec.y, -h/2, h/2);
+      this.vec.z = constrain(this.vec.z, -500, 500);
+
+      let distance = dist(this.vec.x, this.vec.y, mouseX, mouseY);
+  
+      let mouse = createVector(mouseX, mouseY);
+      let difference = p5.Vector.sub(mouse, this.vec);
+      difference.setMag(8);
+      //If the mouse comes near a particle, it moves away
+      if (distance < lineMaxDist) {
+        this.vec.sub(difference);
+      }
+    }
+  }
---
+++ webGL/hogwarts/sketch.js
@@ -0,0 +1,146 @@
+let w = 0
+let h = 0
+let amount = 400
+let vec = 0
+let dots = []
+let lineMaxDist = 100;
+let debug = true
+
+let a = 0
+let inc = 0
+
+let color = [
+  "#436c74", 
+  "#5d9ca8", 
+  "#78cfe0", 
+  "#95dad9", 
+  "#acdac4", 
+  "#bedbad", 
+  "#cddc97", 
+  "#d6de87", 
+  "#cfe198", 
+  "#c7e5a8",
+  "#070605", 
+  "#1f3130", 
+  "#397573", 
+  "#51c1be", 
+  "#93c39e", 
+  "#babb6f", 
+  "#d3b346", 
+  "#dfaa6a", 
+  "#e9a18a", 
+  "#eda3a4"
+]
+
+let hogColor = [
+  "#f6b3b2", 
+  "#f5a8a7", 
+  "#f49d9c", 
+  "#f29192", 
+  "#f08687", 
+  "#f08488", 
+  "#f38d94", 
+  "#f595a0", 
+  "#f69ead", 
+  "#f8a6b9"
+]
+
+let lights = [
+  "#ddf0ef", 
+  "#d5f3ed", 
+  "#cdf6eb", 
+  "#c5f8ea", 
+  "#bcfbe8", 
+  "#bef9df", 
+  "#c9f4d1", 
+  "#d3efc2", 
+  "#dceab3", 
+  "#e4e5a4"
+]
+
+function preload() {
+  hogwarts = loadModel('model/hogwarts.obj');
+}
+
+
+
+/*
+=====================     S E T U P      ========================================
+*/
+
+function setup() {
+  w = windowWidth
+  h = windowHeight
+  var canvas = createCanvas(w, h, WEBGL);
+  canvas.parent('pi5-canvas');
+
+  // debugMode()
+  
+  mainColor = random(color)
+  mainLight = random(lights)
+
+  for (i = 0; i < amount; i++) {
+    let x = random(-w/2,w/2)
+    let y = random(-h/2,h/2)
+    let z = random(-1000,1000)
+    dots[i] = new Particle(x, y, z)
+  }
+  a = 0.0;
+  inc = TWO_PI / 3600.0;
+
+}
+  /*
+  =====================     D R A W      ========================================
+  */
+
+function draw () {
+  // orbitControl()
+  //pointLight(v1, v2, v3, x, y, z)
+  // background(90, 10, 50)
+  background(mainColor)
+  pointLight(mainLight,dots[3].vec.x, dots[3].vec.y, dots[3].vec.z)
+  pointLight(mainLight,dots[0].vec.x, dots[0].vec.y, dots[0].vec.z)
+  // spotLight(255, 255, 0, dots[0].vec.x, dots[0].vec.y, dots[0].vec.z, dots[1].vec.x, dots[1].vec.y, dots[1].vec.z)
+
+
+  for(let i = 0; i < dots.length; i++) {
+    // fill(0)
+    if (i%6 == 0) {
+    dots[i].showFlare()
+    dots[i].updatePos()
+    } else {
+    dots[i].showDot()
+    dots[i].updatePos()
+    }
+    // dots[i].repel()  
+  }
+  noStroke()
+  push()
+  scale(2.2)  // rotateZ(frameCount * 0.01)
+  translate((-w/2)+400, (h/2)-350, +150)
+  rotateY(sin(a))
+  rotateX(HALF_PI)
+  push()
+  // rotateZ(frameCount * 0.001)
+  ambientMaterial(random(hogColor))
+  model(hogwarts);
+  pop()
+  pop()
+
+  a = a + inc
+}
+
+function windowResized() {
+  resizeCanvas(w, h);
+}
+
+function mouseClicked(){
+  if (debug === true){
+    noDebugMode()
+    debug = false
+  } else {
+    debugMode()
+    debug = true
+  }
+  
+}
