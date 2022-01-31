afe09b60917db021b272b48db5d82f6b59422dca
boris-perisset <digital.noize@protonmail.com>
2022-01-31 16:09:08 +0100

    update all

--- webGL/cloud/cloud.js
+++ webGL/cloud/cloud.js
@@ -1,8 +1,6 @@
 class Cloud {
     constructor(x, y, z){
-      this.size = random(4,30) * sin(this.a)
-      this.a = 0.001
-      this.inc = PI/3600
+      this.size = random(4,30)
       this.pos = new p5.Vector(x, y, z)
       this.determination = random(0.05, 0.1)
       this.growing = random()> 0.5 ? true : false
@@ -19,6 +17,7 @@ class Cloud {
     }
 
     deforming() {
+
       let wave = 0.5
       if (this.growing === true) {
       this.size = this.size + this.determination
@@ -30,9 +29,6 @@ class Cloud {
         this.growing = false
       }
      
-
-      this.a = this.a + this.inc
-
       this.pos.x = this.pos.x + random(-wave,wave) 
       this.pos.y = this.pos.y + random(-wave,-wave * (random(1,3)))
       this.pos.z = this.pos.z + random(-wave,wave)
